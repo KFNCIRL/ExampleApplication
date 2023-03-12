c9fe1f9f6237fc0ead0b764c6675ed0b0bb79d2c
KieranFox0394 <kieran.fox.92@gmail.com>
2023-03-12 19:43:30 +0000

    added continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+# Install pm2 and nodejs load balancer
+sudo npm install -g pm2
+# stop any instances
+pm2 stop example_app
+# change dir
+cd ExampleApplication/
+# install dependencies
+npm install
+# start applciation 
+pm2 start ./bin/www --name example_app
